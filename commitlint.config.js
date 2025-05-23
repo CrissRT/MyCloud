0ad50b7bed1977f8654d03901407a87e3f44ec6c
Cristian <smartcristian2002@gmail.com>
2025-05-23 19:18:59 +0300

    refactor: simplify commitlint configuration by removing custom rules

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,11 +1,3 @@
 export default {
-  extends: ['@commitlint/config-conventional'],
-  rules: {
-    'type-enum': [
-      2,
-      'always',
-      ['feat', 'fix', 'docs', 'style', 'refactor', 'perf', 'test', 'build', 'ci', 'chore', 'revert']
-    ],
-    'subject-case': [2, 'always', 'sentence-case']
-  }
+  extends: ['@commitlint/config-conventional']
 };
