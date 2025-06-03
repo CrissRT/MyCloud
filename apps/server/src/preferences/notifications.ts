383a203eeb36bcd14c0310f8bd95f4ba22ee93ad
Cristian <smartcristian2002@gmail.com>
2025-06-03 21:33:50 +0300

    chore: prettier the code

--- apps/server/src/preferences/notifications.ts
+++ apps/server/src/preferences/notifications.ts
@@ -1,24 +1,24 @@
 export enum NotificationType {
-    NEW_PASSWORD = 'newPassword',
-    NEW_IP = 'newIp',
-    NEW_DEVICE_LOGIN = 'newDeviceLogin',
-    SHARED_ITEMS = 'sharedItems',
-    REQUEST_ACCESS = 'requestAccess',
+  NEW_PASSWORD = 'newPassword',
+  NEW_IP = 'newIp',
+  NEW_DEVICE_LOGIN = 'newDeviceLogin',
+  SHARED_ITEMS = 'sharedItems',
+  REQUEST_ACCESS = 'requestAccess'
 }
 
-export interface NotificationPreferences { 
-    id: number;
-    userId: number;
-    newlySharedItems: boolean;
-    requestAccess: boolean;
+export interface NotificationPreferences {
+  id: number;
+  userId: number;
+  newlySharedItems: boolean;
+  requestAccess: boolean;
 }
 
 export interface Notification {
-    id: number;
-    userId: number;
-    type: NotificationType;
-    message: string;
-    read: boolean;
-    createdAt: Date;
-    updatedAt: Date;
+  id: number;
+  userId: number;
+  type: NotificationType;
+  message: string;
+  read: boolean;
+  createdAt: Date;
+  updatedAt: Date;
 }
