373826a5d3986697a0beabc393a01135ae7e17fd
Cristian <smartcristian2002@gmail.com>
2025-06-06 20:34:31 +0300

    feat: add File & Notification interfaces & update database schema

---
+++ apps/server/src/files/file.ts
@@ -0,0 +1,28 @@
+export enum FileUploadStatus {
+  PROCESSING = 'processing',
+  READY = 'ready',
+  ERROR = 'error'
+}
+
+export interface File {
+  id: number;
+  userId: number;
+  parentFolderId: number | null;
+  name: string;
+  originalName: string;
+  filePath: string;
+  mimeType: string;
+  sizeBytes: number;
+  uploadStatus: FileUploadStatus;
+  createdAt: Date;
+  updatedAt: Date;
+  isFavorite: boolean;
+  isDeleted: boolean;
+}
+
+export interface FileSharable {
+  id: number;
+  fileId: number;
+  shareLink: string;
+  shareLinkExpire: Date | null;
+}
