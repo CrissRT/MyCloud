5cb4dfdb6c5c17cfc1d519f5271bc27d7da021e1
Cristian <smartcristian2002@gmail.com>
2025-06-06 22:15:25 +0300

    feat: implement code changes to enhance functionality

---
+++ apps/server/src/i18n/i18n.ts
@@ -0,0 +1,20 @@
+import i18next from 'i18next';
+import Backend from 'i18next-fs-backend';
+import middleware from 'i18next-http-middleware';
+
+i18next
+  .use(Backend)
+  .use(middleware.LanguageDetector)
+  .init({
+    fallbackLng: 'en',
+    preload: ['en', 'ro'],
+    backend: {
+      loadPath: 'src/i18n/locales/{{lng}}.json'
+    },
+    detection: {
+      order: ['querystring', 'cookie', 'header'],
+      caches: ['cookie', 'header']
+    }
+  });
+
+export const i18n = i18next;
