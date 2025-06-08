609d1128d806eb80e9b704976a3ddedec1541522
Cristian <smartcristian2002@gmail.com>
2025-06-08 12:03:32 +0300

    refactor: remove validation logic from UsersService.register method

--- apps/server/src/services/user.ts
+++ apps/server/src/services/user.ts
@@ -1,7 +1,4 @@
-import { TFunction } from 'i18next';
-
 import { User, UserSession } from '@server/models';
-import { BadRequestError } from '@server/utils';
 
 export type UserRegisterParams = Partial<
   Pick<User, 'email' | 'password' | 'sex' | 'firstName' | 'lastName' | 'birthDate'> &
@@ -9,16 +6,5 @@ export type UserRegisterParams = Partial<
 >;
 
 export class UsersService {
-  public register(params: UserRegisterParams, t: TFunction) {
-    const missingFields = Object.entries(params)
-      .filter(([, value]) => !value)
-      .map(([key]) => key);
-    const missingDeviceInfo = missingFields.filter((field) => field === 'deviceInfo' || field === 'ip');
-
-    if (missingFields.length > 0)
-      throw new BadRequestError(t('missingFields', { fields: missingFields.join(', ') }), undefined, missingFields);
-
-    if (missingDeviceInfo.length > 0)
-      throw new BadRequestError(t('missingData', { data: missingDeviceInfo.join(', ') }), missingDeviceInfo);
-  }
+  public register() {}
 }
