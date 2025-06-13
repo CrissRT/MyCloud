cfab91feb7c26699e173294e0574bf5a8ae6f0de
Cristian <smartcristian2002@gmail.com>
2025-06-13 16:16:12 +0300

    refactor: add check for Date instance in object key conversion function

--- apps/server/src/utils/convertors.ts
+++ apps/server/src/utils/convertors.ts
@@ -4,7 +4,7 @@ export const snakeToCamel = (s: string) => s.replace(/_([a-z])/g, (_, letter) =>
 export const convertObjectKeysSnakeCaseToCamelCase = <T>(obj: T): T => {
   if (Array.isArray(obj)) {
     return obj.map((item) => convertObjectKeysSnakeCaseToCamelCase(item)) as T;
-  } else if (obj !== null && typeof obj === 'object') {
+  } else if (obj !== null && typeof obj === 'object' && !(obj instanceof Date)) {
     const result: Record<string, unknown> = {};
     for (const key in obj) {
       if (Object.prototype.hasOwnProperty.call(obj, key)) {
