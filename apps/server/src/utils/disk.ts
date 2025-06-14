737182212967bf3906a0839115798cc94874608d
Cristian <smartcristian2002@gmail.com>
2025-06-14 17:52:05 +0300

    refactor: rename disk space functions for consistency and clarity

--- apps/server/src/utils/disk.ts
+++ apps/server/src/utils/disk.ts
@@ -3,13 +3,13 @@ import checkDiskSpace from 'check-disk-space';
 import { getReservedStorageInMB } from '@server/db';
 import { convertBytesToMB, convertGBToMB } from '@server/utils';
 
-export const getAvailableSpaceinMB = async () => {
+export const getAvailableDiskSpaceinMB = async () => {
   const currentPath = process.cwd(); // Current working dir (where app is running)
   const { free } = await checkDiskSpace(currentPath);
   return convertBytesToMB(free); // Convert bytes to MB
 };
 
-export const getTotalSpaceinMB = async () => {
+export const getTotalDiskSpaceinMB = async () => {
   const currentPath = process.cwd(); // Current working dir (where app is running)
   const { size } = await checkDiskSpace(currentPath);
   return convertBytesToMB(size); // Convert bytes to MB
@@ -17,8 +17,8 @@ export const getTotalSpaceinMB = async () => {
 
 export const checkIfEnoughSpaceInMB = async (requiredSpaceInMB: bigint | number) => {
   const [availablePhysicalMB, totalPhysicalMB, reservedLogicalMB] = await Promise.all([
-    getAvailableSpaceinMB(), // available free space on current disk
-    getTotalSpaceinMB(), // total size of disk where backend is running
+    getAvailableDiskSpaceinMB(), // available free space on current disk
+    getTotalDiskSpaceinMB(), // total size of disk where backend is running
     getReservedStorageInMB() // sum of all allocated storage from DB
   ]);
 
