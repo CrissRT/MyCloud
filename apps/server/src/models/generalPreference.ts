b19d7fabceb22c7b5c49c6cf3eb76ac3345ce5ef
Cristian <smartcristian2002@gmail.com>
2025-06-16 16:34:27 +0300

    refactor: remove unused enum definitions for clarity and maintainability

--- apps/server/src/models/generalPreference.ts
+++ apps/server/src/models/generalPreference.ts
@@ -2,34 +2,6 @@ import { z } from 'zod';
 
 import { $Enums } from '@prisma/client';
 
-export enum Appearance {
-  LIGHT = 'light',
-  DARK = 'dark',
-  DEVICE = 'device'
-}
-
-export enum Language {
-  EN = 'en',
-  RO = 'ro',
-  RU = 'ru'
-}
-
-export enum Density {
-  COMFORTABLE = 'comfortable',
-  COSY = 'cosy',
-  COMPACT = 'compact'
-}
-
-export enum OpenFiles {
-  PREVIEW = 'preview',
-  NEW_TAB = 'newTab'
-}
-
-export enum Layout {
-  LIST = 'list',
-  GRID = 'grid'
-}
-
 export const generalPreferencesSchema = z.object({
   id: z.number().int().nonnegative(),
   userId: z.number().int().nonnegative(),
