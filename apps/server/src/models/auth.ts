ec60c92fa0bbd226239b6d39eb192e286f2be5e7
Cristian <smartcristian2002@gmail.com>
2025-06-18 20:46:30 +0300

    refactor: simplify authResponseSchema by consolidating pick and extend methods

--- apps/server/src/models/auth.ts
+++ apps/server/src/models/auth.ts
@@ -19,19 +19,16 @@ export const userLoginSchema = userSchema.pick({
 });
 export type Login = z.infer<typeof userLoginSchema>;
 
-export const authResponseSchema = userSchema
-  .pick({
+export const authResponseSchema = userSchema.pick({
     email: true,
     username: true,
     firstName: true,
     lastName: true,
     role: true,
     sex: true,
-    birthDate: true
-  })
-  .extend({
-    storageSpaceInMB: z.string().regex(/^\d+$/),
-    usedStorageInBytes: z.string().regex(/^\d+$/)
-  });
+    birthDate: true,
+    storageSpaceInMB: true,
+    usedStorageInBytes: true
+});
 
 export type AuthResponse = z.infer<typeof authResponseSchema>;
