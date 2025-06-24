57ebf7684a594c900d53de14a33c37e4d5b7d6aa
Cristian <smartcristian2002@gmail.com>
2025-06-24 22:18:09 +0300

    feat: enhance registration page with translations and layout

--- apps/client/src/app/[lng]/(auth)/register/page.tsx
+++ apps/client/src/app/[lng]/(auth)/register/page.tsx
@@ -1,5 +1,22 @@
+'use client';
+
+import { useTranslation } from 'react-i18next';
+import { AuthLayout } from '@client/layouts';
+import { routes } from '@client/utils';
+
 const Page = () => {
-  return <div>page</div>;
+  const { t } = useTranslation();
+  return (
+    <AuthLayout
+      title={t('auth.register.createAccount')}
+      description={t('auth.register.joinUs')}
+      redirect={{
+        href: routes.login,
+        description: t('auth.register.alreadyHaveAccount'),
+        linkLabel: t('auth.login.buttons.login')
+      }}
+    ></AuthLayout>
+  );
 };
 
 export default Page;
