1ce00ddca44d717c0b30afeab3b5413cb24c0f7b
Cristian <smartcristian2002@gmail.com>
2025-06-24 22:27:23 +0300

    feat: add registration form with input fields and layout

--- apps/client/src/app/[lng]/(auth)/register/page.tsx
+++ apps/client/src/app/[lng]/(auth)/register/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useTranslation } from 'react-i18next';
+import { Input, Password } from '@client/components';
 import { AuthLayout } from '@client/layouts';
 import { routes } from '@client/utils';
 
@@ -15,7 +16,17 @@ const Page = () => {
         description: t('auth.register.alreadyHaveAccount'),
         linkLabel: t('auth.login.buttons.login')
       }}
-    ></AuthLayout>
+    >
+      <form>
+        <Input label={{ text: t('auth.email') }} input={{ placeholder: t('auth.enterEmail') }} />
+        <Password label={{ text: t('auth.password') }} input={{ placeholder: t('auth.enterPassword') }} />
+        <Password label={{ text: t('auth.confirmPassword') }} input={{ placeholder: t('auth.enterConfirmPassword') }} />
+        <div className="flex gap-4 max-md:gap-0 max-md:flex-col">
+          <Input label={{ text: t('auth.firstName') }} input={{ placeholder: t('auth.enterFirstName') }} size="lg" />
+          <Input label={{ text: t('auth.lastName') }} input={{ placeholder: t('auth.enterLastName') }} size="lg" />
+        </div>
+      </form>
+    </AuthLayout>
   );
 };
 
