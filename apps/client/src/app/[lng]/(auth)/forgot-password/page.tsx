496c4788875f836c93370fab495c48d197923506
Cristian <smartcristian2002@gmail.com>
2025-07-01 22:10:15 +0300

    feat: implement Forgot Password page with form validation and translations

---
+++ apps/client/src/app/[lng]/(auth)/forgot-password/page.tsx
@@ -0,0 +1,57 @@
+'use client';
+
+import { z } from 'zod';
+
+import { useForm } from 'react-hook-form';
+import { useTranslation } from 'react-i18next';
+import { Button, Input } from '@client/components';
+import { AuthLayout } from '@client/layouts';
+import { routes } from '@client/utils';
+import { zodResolver } from '@hookform/resolvers/zod';
+
+const schema = z.object({
+  email: z.string().email()
+});
+
+type ForgotPasswordType = z.infer<typeof schema>;
+
+const Page = () => {
+  const { t } = useTranslation();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm<ForgotPasswordType>({
+    resolver: zodResolver(schema)
+  });
+
+  const onSubmit = async (data: ForgotPasswordType) => {
+    console.log('Forgot password data:', data);
+  };
+
+  return (
+    <AuthLayout
+      title={t('auth.forgotPassword.forgotPassword')}
+      description={t('auth.forgotPassword.enterEmail')}
+      redirect={{
+        description: t('auth.forgotPassword.rememberPassword'),
+        href: routes.login,
+        linkLabel: t('auth.login.buttons.login')
+      }}
+    >
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Input
+          input={{ placeholder: t('auth.enterEmail'), id: 'email', ...register('email') }}
+          label={{ text: t('auth.email') }}
+          error={errors?.email?.message}
+        />
+        <Button width="full" type="submit">
+          {t('auth.forgotPassword.sendResetLink')}
+        </Button>
+      </form>
+    </AuthLayout>
+  );
+};
+
+export default Page;
