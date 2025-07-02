0c429b0b6aaa9d45a313010602ad904658deef99
Cristian <smartcristian2002@gmail.com>
2025-07-02 18:37:05 +0300

    feat: install react-google-sso

--- apps/client/src/app/layout.tsx
+++ apps/client/src/app/layout.tsx
@@ -2,13 +2,14 @@ import { ThemeProvider } from 'next-themes';
 
 import { PropsWithChildren } from 'react';
 import { NotificationContainer } from '@client/components';
-import { PromiseLanguage } from '@client/utils';
+import { getGoogleSSOClientId, PromiseLanguage } from '@client/utils';
 import { config } from '@fortawesome/fontawesome-svg-core';
 
 import '@fortawesome/fontawesome-svg-core/styles.css';
 config.autoAddCss = false;
 
 import { APIProvider, AppI18nextProvider, QueryClientContext } from '@client/contexts';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 import '@client/styles/globals.css';
 import '@client/styles/theme.css';
@@ -24,16 +25,18 @@ const RootLayout = async ({
   return (
     <html lang={lng} suppressHydrationWarning className="h-full w-full min-w-[320px] min-h-full">
       <body className="antialiased w-full h-full text-(--text-primary) min-h-full">
-        <AppI18nextProvider>
-          <QueryClientContext>
-            <APIProvider>
-              <ThemeProvider attribute="data-theme" defaultTheme="system" enableSystem>
-                {children}
-                <NotificationContainer />
-              </ThemeProvider>
-            </APIProvider>
-          </QueryClientContext>
-        </AppI18nextProvider>
+        <GoogleOAuthProvider clientId={getGoogleSSOClientId()}>
+          <AppI18nextProvider>
+            <QueryClientContext>
+              <APIProvider>
+                <ThemeProvider attribute="data-theme" defaultTheme="system" enableSystem>
+                  {children}
+                  <NotificationContainer />
+                </ThemeProvider>
+              </APIProvider>
+            </QueryClientContext>
+          </AppI18nextProvider>
+        </GoogleOAuthProvider>
       </body>
     </html>
   );
