a7f3c429ec2943c0893216ec4d1146aed869e08b
Cristian <smartcristian2002@gmail.com>
2025-07-03 15:09:18 +0300

    refactor: simplify APIProvider by removing unused translation logic and improving cookie handling

--- apps/client/src/contexts/Api.tsx
+++ apps/client/src/contexts/Api.tsx
@@ -1,27 +1,34 @@
 'use client';
 
 import { PropsWithChildren, useEffect } from 'react';
-import { useTranslation } from 'react-i18next';
 import { OpenAPI } from '@client/api/openapi/requests';
+import { i18nConfig } from '@client/i18n/settings';
 import { getAPIBaseUrl } from '@client/utils';
 
-export const APIProvider = ({ children }: PropsWithChildren) => {
-  const { i18n } = useTranslation();
+const getCookie = (name: string) => {
+  const cookieValue = document.cookie
+    .split('; ')
+    .find((row) => row.startsWith(`${name}=`))
+    ?.split('=')[1];
+  return cookieValue;
+};
 
+export const APIProvider = ({ children }: PropsWithChildren) => {
   useEffect(() => {
     OpenAPI.BASE = getAPIBaseUrl();
-    // send HTTP-only cookies with every request
-    OpenAPI.WITH_CREDENTIALS = true;
-    OpenAPI.CREDENTIALS = 'include';
-  }, []);
 
-  useEffect(() => {
     OpenAPI.interceptors.request.use(async (config) => {
+      const language = getCookie('NEXT_LOCALE') || i18nConfig.defaultLocale;
+
       if (!config.headers) config.headers = {};
-      config.headers['Accept-Language'] = i18n.language;
+
+      config.headers['Accept-Language'] = language;
+
+      config.headers['Content-Type'] = 'application/json';
+
       return config;
     });
-  }, [i18n.language]);
+  }, []);
 
   return children;
 };
