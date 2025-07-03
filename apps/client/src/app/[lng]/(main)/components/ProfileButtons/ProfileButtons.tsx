a86e885e0f31e6e108384d38996b5b3e0fc33d24
Cristian <smartcristian2002@gmail.com>
2025-07-03 21:00:13 +0300

    feat: update ProfileButtons to display user's name in the dropdown button

--- apps/client/src/app/[lng]/(main)/components/ProfileButtons/ProfileButtons.tsx
+++ apps/client/src/app/[lng]/(main)/components/ProfileButtons/ProfileButtons.tsx
@@ -4,14 +4,16 @@ import { useRouter } from 'next/navigation';
 
 import { useTranslation } from 'react-i18next';
 import { Dropdown } from '@client/components';
+import { useAuth } from '@client/hooks';
 
 export const ProfileButtons = () => {
   const { t } = useTranslation();
   const router = useRouter();
+  const { user } = useAuth();
 
   return (
     <Dropdown
-      button={{ children: 'asdasd' }}
+      button={{ children: `${user?.firstName} ${user?.lastName}` }}
       options={[
         { label: t('common.profile'), value: 'profile', onClick: () => router.push('/profile') },
         { label: t('common.profilessss'), value: 'profile', onClick: () => router.push('/profile2') }
