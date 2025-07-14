98a6ead7d73afba8b9b50e32d649ff66f18b3c89
Cristian <smartcristian2002@gmail.com>
2025-07-14 21:34:27 +0300

    refactor: add error messages for authentication failures and update session cookie handling

--- apps/server/src/utils/cookie.ts
+++ apps/server/src/utils/cookie.ts
@@ -1,7 +1,11 @@
 import cookie from 'cookie';
 import { NextFunction, Response } from 'express';
 
+import { getUserByEmail } from '@server/db';
 import { AuthCookie, AuthenticatedRequest } from '@server/models';
+import { ErrorCodes } from '@shared/types';
+
+const userEmailCache = new Set<string>();
 
 const maxAgeCookie = 60 * 60 * 24 * 7; // 1 week
 const cookieOptions = {
@@ -19,29 +23,46 @@ export const setCookieHeader = (res: Response, cookieHeader: string) => {
   res.setHeader('Set-Cookie', cookieHeader);
 };
 
-export const authenticateWithCookie = (req: AuthenticatedRequest, res: Response, next: NextFunction): void => {
+export const authenticateWithCookie = async (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
   try {
     const cookies = cookie.parse(req.headers.cookie || '');
     const userSession = cookies.user_session;
 
     if (!userSession) {
-      res.status(401).json({ message: 'Authentication required - no session cookie found' });
+      res.status(401).json({ code: ErrorCodes.INVALID_RECORD, message: req.t('errors.authenticationFailed') });
       return;
     }
 
     // Parse the user session data
     const userData = JSON.parse(userSession);
 
-    //   TODO: add validation searching from DB
     if (!userData) {
-      res.status(401).json({ message: 'Invalid session cookie' });
+      res.status(401).json({ code: ErrorCodes.INVALID_RECORD, message: req.t('errors.invalidSessionCookie') });
       return;
     }
+
+    // Check if the user session is cached
+    if (userEmailCache.has(userData.user.email)) {
+      req.user = userData.user;
+      next();
+      return;
+    }
+
+    // If not cached, fetch user data from the database
+    const userFromDb = await getUserByEmail(userData.user.email);
+
+    if (!userFromDb) {
+      res.status(401).json({ code: ErrorCodes.INVALID_RECORD, message: req.t('errors.invalidSessionCookie') });
+      return;
+    }
+
+    // Cache the user email
+    userEmailCache.add(userFromDb.email);
 
     // Attach user data to request for use in route handlers
     req.user = userData.user;
     next();
   } catch {
-    res.status(401).json({ message: 'Invalid session cookie format' });
+    res.status(401).json({ code: ErrorCodes.INVALID_RECORD, message: req.t('errors.invalidSessionCookie') });
   }
 };
