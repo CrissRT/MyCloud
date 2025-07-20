a89be8bd6878f3f6383fdaa65c9d765be80cb01d
CrissRT <smartcristian2002@gmail.com>
2025-07-20 16:32:29 +0300

    chore: prettier the code

--- apps/client/src/api/openapi/queries/common.ts
+++ apps/client/src/api/openapi/queries/common.ts
@@ -1,14 +1,36 @@
-// generated with @7nohe/openapi-react-query-codegen@1.6.2 
+// generated with @7nohe/openapi-react-query-codegen@1.6.2
 
-import { UseQueryResult } from "@tanstack/react-query";
-import { ForgotPasswordService, GoogleService, LoginService, MeService, PreferencesService, RegisterService, ResetPasswordService } from "../requests/services.gen";
+import { UseQueryResult } from '@tanstack/react-query';
+import {
+  ForgotPasswordService,
+  GoogleService,
+  LoginService,
+  MeService,
+  PreferencesService,
+  RegisterService,
+  ResetPasswordService
+} from '../requests/services.gen';
 export type MeServiceGetAccountMeDefaultResponse = Awaited<ReturnType<typeof MeService.getAccountMe>>;
-export type MeServiceGetAccountMeQueryResult<TData = MeServiceGetAccountMeDefaultResponse, TError = unknown> = UseQueryResult<TData, TError>;
-export const useMeServiceGetAccountMeKey = "MeServiceGetAccountMe";
-export const UseMeServiceGetAccountMeKeyFn = (queryKey?: Array<unknown>) => [useMeServiceGetAccountMeKey, ...(queryKey ?? [])];
-export type RegisterServicePostAuthRegisterMutationResult = Awaited<ReturnType<typeof RegisterService.postAuthRegister>>;
+export type MeServiceGetAccountMeQueryResult<
+  TData = MeServiceGetAccountMeDefaultResponse,
+  TError = unknown
+> = UseQueryResult<TData, TError>;
+export const useMeServiceGetAccountMeKey = 'MeServiceGetAccountMe';
+export const UseMeServiceGetAccountMeKeyFn = (queryKey?: Array<unknown>) => [
+  useMeServiceGetAccountMeKey,
+  ...(queryKey ?? [])
+];
+export type RegisterServicePostAuthRegisterMutationResult = Awaited<
+  ReturnType<typeof RegisterService.postAuthRegister>
+>;
 export type LoginServicePostAuthLoginMutationResult = Awaited<ReturnType<typeof LoginService.postAuthLogin>>;
-export type ForgotPasswordServicePostAuthForgotPasswordMutationResult = Awaited<ReturnType<typeof ForgotPasswordService.postAuthForgotPassword>>;
-export type ResetPasswordServicePostAuthResetPasswordMutationResult = Awaited<ReturnType<typeof ResetPasswordService.postAuthResetPassword>>;
+export type ForgotPasswordServicePostAuthForgotPasswordMutationResult = Awaited<
+  ReturnType<typeof ForgotPasswordService.postAuthForgotPassword>
+>;
+export type ResetPasswordServicePostAuthResetPasswordMutationResult = Awaited<
+  ReturnType<typeof ResetPasswordService.postAuthResetPassword>
+>;
 export type GoogleServicePostAuthGoogleMutationResult = Awaited<ReturnType<typeof GoogleService.postAuthGoogle>>;
-export type PreferencesServicePatchAccountPreferencesMutationResult = Awaited<ReturnType<typeof PreferencesService.patchAccountPreferences>>;
+export type PreferencesServicePatchAccountPreferencesMutationResult = Awaited<
+  ReturnType<typeof PreferencesService.patchAccountPreferences>
+>;
